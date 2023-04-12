9ae0f03d96d34ea3d1a290886b5f1765eecbd25e
Colton <allen.colton@hotmail.com>
2023-04-11 18:59:35 -0700

    connected IM slackbot

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 import fetch from 'node-fetch';
 import { GoogleSpreadsheet } from 'google-spreadsheet';
+import pkg from '@slack/bolt';
+const { App } = pkg;
 import dotenv from 'dotenv';
 dotenv.config();
 // import './config/graphic-tide-340702-00aaae7a357a.json';
 const doc = new GoogleSpreadsheet('1alLfPeuZUmVsNUA6nfrOwaKNzd0GHM1UL99dS4c51v8');
 
-// console.log(process.env.GOOGLE_PRIVATE_KEY);
+const app = new App({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  token: process.env.SLACK_BOT_TOKEN,
+});
 
+const channels = {
+  sitehealth: 'C038G7KMZ3J',
+  pingbotLog: 'C0390N5HRNX'
+}
 
-const doStuff = async () => {
+const pingSalonUrls = async () => {
   // await doc.useServiceAccountAuth(creds);
   await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
@@ -36,8 +45,23 @@ const doStuff = async () => {
     const formattedUrl = url.replace('https://').replace('https//').replace('www.', '');
     const res = await fetch(`https://${formattedUrl}`);
     console.log(`${res.status}`)
+    if (res.status !== 200) {
+      sendSlackMessage(channels['sitehealth'], `${url} responded with a status code of ${res.status}`)
+    }
   }
-  
-  // console.log(salons[1].Domain)
+
 }
-doStuff();
+pingSalonUrls();
+
+const sendSlackMessage = async (channel, msg) => {
+  try {
+    // Call the app.chat.postMessage method using the WebClient
+    const result = await app.client.chat.postMessage({
+      channel: channel,
+      text: msg
+    });
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
